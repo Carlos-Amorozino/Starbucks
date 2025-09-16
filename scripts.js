0b2fc8d1d133e3fd14451f4fa907f53bd863f0ff
Carlos-Amorozino <eleniz10mei@gmail.com>
2025-09-15 21:50:47 -0300

    Starbucks

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let imagem = document.querySelector('.copo-maior')
+let fundo = document.querySelector(".circulo")
+
+function trocarImagem(endereco){
+    imagem.src = endereco
+}
+
+function trocaFundo(cor){
+    fundo.style.background = cor
+}
+
+
